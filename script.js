78e6a9ea0f5da43ef2a7dbc3bfaa6367e05a2258
abrahan <abrahandev@gmail.com>
2023-10-16 10:21:04 +0200

    script.js addEventListeners

--- script.js
+++ script.js
@@ -14,5 +14,29 @@ document.addEventListener('DOMContentLoaded', () => {
         gridView.classList.remove('active')
         listView.classList.add('active')
         projectList.classList.remove('change-mode')
+        projectList.classList.add('jsListView')
+    })
+    gridView.addEventListener('click', () => {
+        gridView.classList.add('active')
+        listView.classList.remove('active')
+        projectList.classList.remove('jsListView')
+        projectList.classList.add('change-mode')
+    })
+
+    document.querySelector('.service-btn').addEventListener('click', () => {
+        document.querySelector('.data-section').classList.add('show')
+    })
+    document.querySelector('.data-close').addEventListener('click', () => {
+        document.querySelector('.data-section').classList.remove('show')
+    })
+    document.querySelector('.spen-side').addEventListener('click', () => {
+        document.querySelector('.sidebar').classList.add('show')
+        document.querySelector('close-side').classList.add('show')
+    })
+    document.querySelector('.close-side').addEventListener('click', () => {
+        document.querySelector('.sidebar').classList.remove('show')
+        document.querySelector('close-side').classList.remove('show')
     })
 })
+
+19: 48
